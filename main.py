3f973371a7e2b52d1f74dcca6cf185c503f60958
atrm3 <truonganthony64@gmail.com>
2024-10-08 00:33:30 -0500

    Add incorrect input catches and show single event

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ async def on_ready() -> None:
     except Exception as e:
         print(e)
 
-@bot.tree.command(name="schedule_day")
+#Schedules an event for a single day
+@bot.tree.command(name="schedule_day", description= "Hours must be in 24 hours time and earliest and latest start time must be within 12 hours")
 async def schedule_day(interaction, earliest_start_time: int, latest_start_time: int, month: int, day : int, event_name: str) -> None:
     global all_schedule_events
     id_number = len(all_schedule_events) + 1
@@ -28,15 +29,22 @@ async def schedule_day(interaction, earliest_start_time: int, latest_start_time:
     times = []
 
     #Make sure that the start and end time are within 24
-    if earliest_start_time < 0:
-        earliest_start_time = 0
+    if earliest_start_time < 0 or earliest_start_time > 23:
+        await interaction.response.send_message("Please enter a valid earliest starting time")
+        return
 
-    if latest_start_time > 24:
-        latest_start_time = 24
+    if latest_start_time > 24 or latest_start_time < 1:
+        await interaction.response.send_message("Please enter a valid latest starting time")
+        return
 
-    if earliest_start_time + latest_start_time > 12 or earliest_start_time >= latest_start_time:
-        latest_start_time = earliest_start_time + 12
+    if earliest_start_time + 12 < latest_start_time:
+        await interaction.response.send_message("Latest start time cannot be more than earliest start time by 13 or more")
+        return
 
+    if earliest_start_time >= latest_start_time:
+        await interaction.response.send_message("Earliest start time can not be more or equal than latest start time")
+        return
+    
     #Create the options
     for i in range(earliest_start_time, latest_start_time):
         times.append(discord.SelectOption(label= str(i)+":00"))
@@ -78,44 +86,69 @@ async def schedule_day(interaction, earliest_start_time: int, latest_start_time:
     await interaction.response.send_message("Choose times when you are avaliable for "+ event_name +" on "+ str(month) + "/"+ str(day), view=view)
     return
 
+#Show the availability of users for a certain event
+@bot.tree.command(name="show_availability_day")
+async def show_availability_day(interaction, event_name: str) -> None:
+    global all_schedule_events
+    message = event_name + " could not be found"
+    for event in range(len(all_schedule_events)):
+        if all_schedule_events[event]["event_name"] == event_name:
+            #Construct the message to be sent
+            message = "```Possible Starting Time | Date  | Event Name\n"
+            date = str(event["month"]) +"/" + str(event["day"])
+            possible_stating_time = event["earliest_start_time"]+ " - " + event["latest_start_time"]
+            message += (possible_stating_time.ljust(23)+ "| " + date.ljust(5) + " | " + event["event_name"]+"\n")
+            message += "```"
+            break
+    await interaction.response.send_message(message)
+    return
+
+#Show all events and their respective information
 @bot.tree.command(name="show_all_schedule_event_day")
 async def show_all_schedule_event_day(interaction) -> None:
     global all_schedule_events
 
     #If there are no events say there is no events
     if all_schedule_events == []:
-        await interaction.response.send_message("No event has been scheduled")
+        await interaction.response.send_message("No events have been scheduled")
         return
 
-    message = ("```Possible Starting Time | Date | Event Name\n")
+    #Format the message to be sent
+    message = ("```Possible Starting Time | Date  | Event Name\n")
 
     for event in all_schedule_events:
         date = str(event["month"]) +"/" + str(event["day"])
         possible_stating_time = event["earliest_start_time"]+ " - " + event["latest_start_time"]
-        message += (possible_stating_time.ljust(23)+ "| " + date.ljust(4) + " | " + event["event_name"]+"\n")
+        message += (possible_stating_time.ljust(23)+ "| " + date.ljust(5) + " | " + event["event_name"]+"\n")
     
     #Add quotes so dicord actually spaces thing evenly
     message += "```"
     await interaction.response.send_message(message)
     return
 
+#Clear all schedule event
 @bot.tree.command(name="delete_all_schedule_event_day")
 async def delete_all_schedule_event_day(interaction) -> None:
     global all_schedule_events
     all_schedule_events = []
+    await interaction.response.send_message("All events have been deleted")
     return
 
+#Delete a single event
 @bot.tree.command(name="delete_schedule_event_day")
 async def delete_all_schedule_event_day(interaction, event_name: str) -> None:
     global all_schedule_events
-    for event in all_schedule_events:
+    message = event_name +" could not be found"
+    for event in range(len(all_schedule_events)):
         if all_schedule_events[event]["event_name"] == event_name:
-            all_schedule_events.remove(event)
+            all_schedule_events.pop(event)
+            message = event_name+" has been deleted"
             break
+    await interaction.response.send_message(message)
     return
 
 def main() -> None:
-    bot.run("TOKENGOESHERE")
+    bot.run("Token goes here")
     return
 
 if __name__ == "__main__": 
